4577246abfc7b1cd31101b14b4514f1af395f527
Jean-Christophe GAY <contact@jeanchristophegay.com>
2015-04-12 16:38:26 +0200

    Add example for Jetty 7

---
+++ examples/jetty-7/docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$1" == "build" ]; then
+    version=`xmlstarlet sel -N ns="http://maven.apache.org/POM/4.0.0" -t -c "/ns:project/ns:version/text()" -n ../../pom.xml`
+    webapp='../example-webapp'
+    rm *.jar *.war .send-notification
+    mvn clean package -f ../../pom.xml &&
+    cp ../../jetty-notifier/target/jetty-notifier-$version-shaded.jar jetty-notifier.jar
+    mvn clean package -f $webapp/pom.xml &&
+    cp $webapp/target/example-webapp.war . &&
+    cp ../.send-notification . &&
+    sed -i '' 's/#apikey#/'$2'/g;s/#device#/'$3'/g' .send-notification &&
+    docker build -t="jcgay/example-webapp-jetty-7" .
+elif [ "$1" == "run" ]; then
+    docker run -d -p 8080:8080 jcgay/example-webapp-jetty-7
+else
+    echo "Try again: <$0 build> OR <$0 run>"
+fi
